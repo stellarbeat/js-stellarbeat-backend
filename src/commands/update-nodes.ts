2dc73205fe026f86a45f4113f22fbf2f58147a3e
pieterjan84 <pieterjan84@users.noreply.github.com>
2019-04-17 07:49:57 +0200

    remove duplicate public keys moved to backend

--- src/commands/update-nodes.ts
+++ src/commands/update-nodes.ts
@@ -29,6 +29,8 @@ async function run() {
         let nodes = await crawler.crawl(nodesSeed);
         nodes = nodes.filter(node => node.publicKey); //filter out nodes without public keys
 
+        nodes = removeDuplicatePublicKeys(nodes);
+
         console.log("[MAIN] Starting map to stellar dashboard information");
         nodes = await mapStellarDashboardNodes(nodes);
 
@@ -151,4 +153,62 @@ async function archiveToS3(nodes: Node[]) {
     });
 
     return await s3.upload(params as any).promise();
+}
+
+function removeDuplicatePublicKeys(nodes: Node[]):Node[] {
+    //filter out double public keys (nodes that switched ip address, or have the same public key running on different ip's at the same time)
+    //statistics are lost because new ip
+    let publicKeys = nodes.map((node:Node) => node.publicKey);
+    let duplicatePublicKeys:string[] = [];
+    publicKeys.forEach((element, index) => {
+
+        // Find if there is a duplicate or not
+        if (publicKeys.indexOf(element, index + 1) > -1) {
+
+            // Is the duplicate already registered?
+            if (duplicatePublicKeys.indexOf(element) === -1) {
+                duplicatePublicKeys.push(element);
+            }
+        }
+    });
+
+    duplicatePublicKeys.forEach(duplicatePublicKey => {
+        console.log('duplicate publicKey: ' + duplicatePublicKey);
+        let duplicateNodes = nodes.filter(node => node.publicKey === duplicatePublicKey);
+
+        let nodeToKeep = duplicateNodes[0];
+        let nodesToDiscard = [];
+        for(let i=1; i<duplicateNodes.length; i++) {
+            if(duplicateNodes[i].dateDiscovered > nodeToKeep.dateDiscovered) {
+                nodesToDiscard.push(nodeToKeep);
+                nodeToKeep = duplicateNodes[i];
+            } else {
+                nodesToDiscard.push(duplicateNodes[i]);
+            }
+        }
+
+        let nodeWithName = duplicateNodes.find(node => node.name !== undefined && node.name !== null );
+        if(nodeWithName !== undefined) {
+            nodeToKeep.name = nodeWithName.name;
+        }
+
+        let nodeWithHost = duplicateNodes.find(node => node.host !== undefined && node.host !== null);
+        if(nodeWithHost !== undefined) {
+            nodeToKeep.host = nodeWithHost.host;
+        }
+
+        let nodeWithGeoData = duplicateNodes.find(node => node.geoData.longitude !== undefined && node.geoData.longitude !== null);
+        if(nodeWithGeoData !== undefined) {
+            nodeToKeep.geoData = nodeWithGeoData.geoData;
+        }
+
+        nodesToDiscard.forEach(nodeToDiscard => {
+            let index = nodes.indexOf(nodeToDiscard);
+            if (index > -1) {
+                nodes.splice(index, 1);
+            }
+        });
+    });
+
+    return nodes;
 }
