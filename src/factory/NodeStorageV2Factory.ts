a9c92c0b5c5348bf0ac10e3d2cac3e551870630b
pieterjan84 <pieterjan84@users.noreply.github.com>
2019-12-08 10:31:55 +0100

    created NodeStorageV2Factory

---
+++ src/factory/NodeStorageV2Factory.ts
@@ -0,0 +1,21 @@
+import {Node, Organization} from "@stellarbeat/js-stellar-domain";
+import CrawlV2 from "../entities/CrawlV2";
+import NodeSnapShotFactory from "./NodeSnapShotFactory";
+import NodeStorageV2 from "../entities/NodeStorageV2";
+
+export default class NodeStorageV2Factory {
+    protected nodeSnapShotFactory: NodeSnapShotFactory;
+
+    constructor(nodeSnapShotFactory:NodeSnapShotFactory){
+        this.nodeSnapShotFactory = nodeSnapShotFactory;
+    }
+
+    create(node: Node, crawl: CrawlV2, organization?: Organization){
+        let nodeV2Storage = new NodeStorageV2(node.publicKey);
+        let nodeSnapshot = this.nodeSnapShotFactory.create(nodeV2Storage, node, crawl, organization);
+
+        nodeV2Storage.snapShots.push(nodeSnapshot);
+
+        return nodeV2Storage;
+    }
+}
