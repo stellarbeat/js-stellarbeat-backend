d6eb2f6e19e46c03b18f1427bc266e7ce686072b
pieterjan84 <pieterjan84@users.noreply.github.com>
2020-01-05 21:51:07 +0100

    removed test tryout

--- test/entities/OrganizationSnapShot.test.ts
+++ test/entities/OrganizationSnapShot.test.ts
@@ -84,14 +84,4 @@ describe("organization snapshot changed", () => {
 
         expect(organizationSnapShot.organizationChanged(organization)).toBeFalsy();
     });
-
-    test('weird', () => {
-        let organization = Organization.fromJSON('{"id": "2c94d134d481fe31dadf9014b274c73f", "dba": "Future Tense", "url": "https://futuretense.io", "name": "Future Tense, LLC", "github": "https:&#x2F;&#x2F;github.com&#x2F;future-tense", "validators": ["GBFZFQRGOPQC5OEAWO76NOY6LBRLUNH4I5QYPUYAK53QSQWVTQ2D4FT5"], "officialEmail": "hello@futuretense.io", "subQuorum30DaysAvailability": 100, "subQuorum24HoursAvailability": 100}');
-        let snapShot = organizationSnapShotFactory.create(new OrganizationIdStorage("2c94d134d481fe31dadf9014b274c73f", new Date()), organization!, new CrawlV2(), [new NodePublicKeyStorage('GBFZFQRGOPQC5OEAWO76NOY6LBRLUNH4I5QYPUYAK53QSQWVTQ2D4FT5')] )
-
-        let nextCrawl = Organization.fromJSON('{"id": "2c94d134d481fe31dadf9014b274c73f", "dba": "Future Tense", "url": "https://futuretense.io", "name": "Future Tense, LLC", "github": "https:&#x2F;&#x2F;github.com&#x2F;future-tense", "validators": ["GBFZFQRGOPQC5OEAWO76NOY6LBRLUNH4I5QYPUYAK53QSQWVTQ2D4FT5"], "officialEmail": "hello@futuretense.io", "subQuorum30DaysAvailability": 99.33, "subQuorum24HoursAvailability": 99.33}');
-
-        expect(snapShot.organizationChanged(nextCrawl!)).toBeFalsy();
-
-    })
 });
