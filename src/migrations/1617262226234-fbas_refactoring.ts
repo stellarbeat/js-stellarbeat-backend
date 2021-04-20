f733d7795da6b42283b0cc88142b76f09daa4db3
pieterjan84 <pieterjan84@users.noreply.github.com>
2021-04-20 10:53:14 +0200

    updated db migration file

--- src/migrations/1617262226234-fbas_refactoring.ts
+++ src/migrations/1617262226234-fbas_refactoring.ts
@@ -1,7 +1,7 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
 export class fbasCleanup1617262226234 implements MigrationInterface {
-    name = 'fbasCleanup1617262226234'
+    name = 'fbasRefactoring1617262226234'
 
     public async up(queryRunner: QueryRunner): Promise<any> {
         await queryRunner.query(`ALTER TABLE "network_measurement" DROP COLUMN "topTierFilteredSize"`, undefined);
@@ -35,6 +35,34 @@ export class fbasCleanup1617262226234 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "network_measurement_month" DROP COLUMN "topTierOrgsFilteredSum"`, undefined);
         await queryRunner.query(`ALTER TABLE "network_measurement_month" DROP COLUMN "minSplittingSetFilteredSum"`, undefined);
         await queryRunner.query(`ALTER TABLE "network_measurement_month" DROP COLUMN "minSplittingSetOrgsFilteredSum"`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement" ADD "minBlockingSetCountrySize" smallint NOT NULL DEFAULT 0`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement" ADD "minBlockingSetCountryFilteredSize" smallint NOT NULL DEFAULT 0`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement" ADD "minBlockingSetISPSize" smallint NOT NULL DEFAULT 0`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement" ADD "minBlockingSetISPFilteredSize" smallint NOT NULL DEFAULT 0`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement_day" ADD "minBlockingSetCountryMin" smallint NOT NULL DEFAULT 0`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement_day" ADD "minBlockingSetCountryMax" smallint NOT NULL DEFAULT 0`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement_day" ADD "minBlockingSetCountrySum" integer NOT NULL DEFAULT 0`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement_day" ADD "minBlockingSetCountryFilteredMin" smallint NOT NULL DEFAULT 0`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement_day" ADD "minBlockingSetCountryFilteredMax" smallint NOT NULL DEFAULT 0`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement_day" ADD "minBlockingSetCountryFilteredSum" integer NOT NULL DEFAULT 0`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement_day" ADD "minBlockingSetISPMin" smallint NOT NULL DEFAULT 0`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement_day" ADD "minBlockingSetISPMax" smallint NOT NULL DEFAULT 0`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement_day" ADD "minBlockingSetISPSum" integer NOT NULL DEFAULT 0`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement_day" ADD "minBlockingSetISPFilteredMin" smallint NOT NULL DEFAULT 0`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement_day" ADD "minBlockingSetISPFilteredMax" smallint NOT NULL DEFAULT 0`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement_day" ADD "minBlockingSetISPFilteredSum" integer NOT NULL DEFAULT 0`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement_month" ADD "minBlockingSetCountryMin" smallint NOT NULL DEFAULT 0`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement_month" ADD "minBlockingSetCountryMax" smallint NOT NULL DEFAULT 0`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement_month" ADD "minBlockingSetCountrySum" integer NOT NULL DEFAULT 0`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement_month" ADD "minBlockingSetCountryFilteredMin" smallint NOT NULL DEFAULT 0`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement_month" ADD "minBlockingSetCountryFilteredMax" smallint NOT NULL DEFAULT 0`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement_month" ADD "minBlockingSetCountryFilteredSum" integer NOT NULL DEFAULT 0`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement_month" ADD "minBlockingSetISPMin" smallint NOT NULL DEFAULT 0`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement_month" ADD "minBlockingSetISPMax" smallint NOT NULL DEFAULT 0`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement_month" ADD "minBlockingSetISPSum" integer NOT NULL DEFAULT 0`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement_month" ADD "minBlockingSetISPFilteredMin" smallint NOT NULL DEFAULT 0`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement_month" ADD "minBlockingSetISPFilteredMax" smallint NOT NULL DEFAULT 0`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement_month" ADD "minBlockingSetISPFilteredSum" integer NOT NULL DEFAULT 0`, undefined);
     }
 
     public async down(queryRunner: QueryRunner): Promise<any> {
@@ -69,6 +97,34 @@ export class fbasCleanup1617262226234 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "network_measurement_month" ADD "minSplittingSetOrgsFilteredMin" smallint NOT NULL DEFAULT 0`, undefined);
         await queryRunner.query(`ALTER TABLE "network_measurement_month" ADD "minSplittingSetOrgsFilteredMax" smallint NOT NULL DEFAULT 0`, undefined);
         await queryRunner.query(`ALTER TABLE "network_measurement_month" ADD "minSplittingSetOrgsFilteredSum" integer NOT NULL DEFAULT 0`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement_month" DROP COLUMN "minBlockingSetISPFilteredSum"`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement_month" DROP COLUMN "minBlockingSetISPFilteredMax"`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement_month" DROP COLUMN "minBlockingSetISPFilteredMin"`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement_month" DROP COLUMN "minBlockingSetISPSum"`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement_month" DROP COLUMN "minBlockingSetISPMax"`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement_month" DROP COLUMN "minBlockingSetISPMin"`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement_month" DROP COLUMN "minBlockingSetCountryFilteredSum"`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement_month" DROP COLUMN "minBlockingSetCountryFilteredMax"`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement_month" DROP COLUMN "minBlockingSetCountryFilteredMin"`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement_month" DROP COLUMN "minBlockingSetCountrySum"`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement_month" DROP COLUMN "minBlockingSetCountryMax"`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement_month" DROP COLUMN "minBlockingSetCountryMin"`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement_day" DROP COLUMN "minBlockingSetISPFilteredSum"`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement_day" DROP COLUMN "minBlockingSetISPFilteredMax"`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement_day" DROP COLUMN "minBlockingSetISPFilteredMin"`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement_day" DROP COLUMN "minBlockingSetISPSum"`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement_day" DROP COLUMN "minBlockingSetISPMax"`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement_day" DROP COLUMN "minBlockingSetISPMin"`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement_day" DROP COLUMN "minBlockingSetCountryFilteredSum"`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement_day" DROP COLUMN "minBlockingSetCountryFilteredMax"`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement_day" DROP COLUMN "minBlockingSetCountryFilteredMin"`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement_day" DROP COLUMN "minBlockingSetCountrySum"`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement_day" DROP COLUMN "minBlockingSetCountryMax"`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement_day" DROP COLUMN "minBlockingSetCountryMin"`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement" DROP COLUMN "minBlockingSetISPFilteredSize"`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement" DROP COLUMN "minBlockingSetISPSize"`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement" DROP COLUMN "minBlockingSetCountryFilteredSize"`, undefined);
+        await queryRunner.query(`ALTER TABLE "network_measurement" DROP COLUMN "minBlockingSetCountrySize"`, undefined);
     }
 
 
