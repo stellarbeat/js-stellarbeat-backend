b18460379f9c27e420c8f609ce1f1133d69af034
pieterjan84 <pieterjan84@users.noreply.github.com>
2021-09-29 09:37:44 +0200

    migration added default value

--- src/migrations/1632900021069-active-in-scp.ts
+++ src/migrations/1632900021069-active-in-scp.ts
@@ -4,7 +4,7 @@ export class activeInScp1632900021069 implements MigrationInterface {
     name = 'activeInScp1632900021069'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "public"."node_measurement_v2" ADD "isActiveInScp" boolean NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "public"."node_measurement_v2" ADD "isActiveInScp" boolean NOT NULL DEFAULT FALSE`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
