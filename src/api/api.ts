dbc681e38255bf8a9c25c58e3bdb616899dd985a
pieterjan84 <pieterjan84@users.noreply.github.com>
2021-11-25 12:06:43 +0100

    CORS: allowing DELETE

--- src/api/api.ts
+++ src/api/api.ts
@@ -93,6 +93,10 @@ const listen = async () => {
 			'Access-Control-Allow-Headers',
 			'Origin, X-Requested-With, Content-Type, Accept'
 		);
+		res.header(
+			'Access-Control-Allow-Methods',
+			'GET, POST, PUT, DELETE, OPTIONS'
+		);
 		next();
 	});
 
